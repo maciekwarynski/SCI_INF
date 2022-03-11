7ac444c67c90068f630f6fa4bcf72e2230297f46
maciekwarynski <91892528+maciekwarynski@users.noreply.github.com>
2022-03-11 16:37:40 +0100

    MyVector

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include "klasa.h"
+using namespace std;
+
+int main()
+{
+	MyVector Vector;
+	Vector.add(3);
+	Vector.printTable();
+	cout << "--------------------" << endl;
+	Vector.remove();
+	Vector.printTable();
+	cout << "--------------------" << endl;
+	Vector.addAt(5, 2);
+	Vector.printTable();
+	cout << "--------------------" << endl;
+	Vector.removeAt(5);
+	Vector.printTable();
+	cout << "--------------------" << endl;
+	Vector.addAt(1, 8);
+	Vector.printAt(1);
+
+}
